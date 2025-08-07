487b85943e18373ddce9396c36285fc437769de9
Ana Trkulja <mejladresa222@gmail.com>
2025-08-07 09:50:06 +0200

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </head>
       <body>
         <AuthProvider>
           {children}
